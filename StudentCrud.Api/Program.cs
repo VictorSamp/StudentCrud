0f9a1c27ad5d20849d59802a5ae29304e38ebe85
VictorSamp <victor.msampaio@outlook.com.br>
2024-05-11 15:26:35 -0300

    feat: add api dockerfile

--- StudentCrud.Api/Program.cs
+++ StudentCrud.Api/Program.cs
@@ -24,13 +24,8 @@ builder.Services.AddDbContext<StudentCrudReadModelContext>(
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
-
+app.UseSwagger();
+app.UseSwaggerUI();
 app.UseAuthorization();
 app.MapControllers();
 app.Run();
